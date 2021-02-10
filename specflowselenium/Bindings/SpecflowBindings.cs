32d19bb3dac3ca409867d669ef1c5e2fe13131b6
Neeraja Bolegowda <b.niraja@gmail.com>
2021-02-10 18:17:57 +0100

    Completed FinishMe scenario.

--- specflowselenium/Bindings/SpecflowBindings.cs
+++ specflowselenium/Bindings/SpecflowBindings.cs
@@ -3,23 +3,56 @@
     using OpenQA.Selenium;
     using OpenQA.Selenium.Firefox;
     using TechTalk.SpecFlow;
+    using specflowselenium.Utils;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [Binding]
     class SpecflowBindings
     {
-        private IWebDriver Driver;
+        private IWebDriver driver;
 
         [When(@"I start the browser")]
         public void WhenIStartTheBrowser()
         {
-            Driver = new FirefoxDriver();
+            driver = new FirefoxDriver();
         }
 
         [When(@"I navigate to '(.*)'")]
         public void WhenINavigateToHttpExample_Com(string Url)
         {
-            Driver.Navigate().GoToUrl(Url);
+            driver.Navigate().GoToUrl(Url);
         }
 
+
+        [When(@"I click on the '(.*)'")]
+        public void WhenIClickOnMoreInfoLink(string moreInfoLink)
+        {
+            driver.FindElement(By.LinkText(moreInfoLink)).Click();
+        }
+
+        [Then(@"a link with text '(.*)' must be present")]
+        public void ThenALinkWithTextMustBePresent(string linkText)
+        {
+            Assert.IsTrue(driver.FindElement(By.LinkText(linkText)).Displayed, $"A link with text {linkText} is not present");
+        }
+
+        [Then(@"the '(.*)' box must contain '(.*)' at index '(.*)'")]
+        public void ThenTheBoxMustContainLinkAtGivenIndex(string domainNameBox, string linkName, int index)
+        {
+            // Check for domainbox header
+            var domainNameListBoxElement = WebDriverUtilities.FindElement(driver, By.XPath($"//div[@id='sidebar_left']/div[@class='navigation_box']/h2"), 5);
+            Assert.AreEqual(domainNameListBoxElement.Text, domainNameBox, true, $"Domain Box List can not find the header {domainNameBox}");
+
+            // check for the link
+            var domainNameList2ndElement = WebDriverUtilities.FindElement(driver, By.XPath($"//div[@id='sidebar_left']/div[@class='navigation_box']/ul/li[{index}]"), 5);
+            Assert.AreEqual(domainNameList2ndElement.Text, linkName, true, $"Domain Box List can not find the link {linkName}");
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            // Dispose the browser after each scenario
+            driver.Dispose();
+        }
     }
 }
---
+++ specflowselenium/Utils/WebDriverUtilities.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace specflowselenium.Utils
+{
+    /// <summary>
+    /// WebDriver utlities class for common helper methods.
+    /// </summary>
+    public static class WebDriverUtilities
+    {
+        /// <summary>
+        /// Helper method to wait until element is found.
+        /// </summary>
+        /// <param name="driver">Selenium web driver.</param>
+        /// <param name="by">element to be found using by</param>
+        /// <param name="timeoutInSeconds">time out in seconds</param>
+        /// <returns>Element found result with timeout.</returns>
+        public static IWebElement FindElement(IWebDriver driver, By by, int timeoutInSeconds)
+        {
+            if (timeoutInSeconds > 0)
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+                return wait.Until(drv => drv.FindElement(by));
+            }
+
+            return driver.FindElement(by);
+        }
+    }
+}
